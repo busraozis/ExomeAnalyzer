ad94c53fc064aabdc12bc867b7a226c2b0f00d0f
bozis <busraozis1994@hotmail.com>
2017-04-01 13:05:27 +0300

    ilk pencere denemeleri

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+#simple gui
+from tkinter import *
+
+#create window
+root = Tk()
+
+#modify root window
+root.title("Exome Analyzer")
+root.geometry("800x500")
+
+but = Button(root, text="Start Simulation", width=25)
+but.pack()
+
+root.mainloop()
+
