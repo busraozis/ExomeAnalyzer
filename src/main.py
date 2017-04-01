78f21de5d3ae3e46298f096b2ffe30c3115e168d
bozis <busraozis1994@hotmail.com>
2017-04-01 13:36:15 +0300

    ilk pencere denemeleri

--- src/main.py
+++ src/main.py
@@ -1,15 +1,42 @@
 #simple gui
 from tkinter import *
 
+class GUI(Tk):
+
+    def __init__(self,master=None):
+        Tk.__init__(self, master)
+        self.grid()
+
+        self.submitButton = Button(master, text="Start Simulation", width=25, bg = "#009f9a", command=self.buttonClick)
+        self.submitButton.grid()
+        self.submitButton.pack()
+        self.submitButton.place(width=250, relx = 0.365, rely=0.5)
+
+        self.title("Exome Analyzer")
+        self.geometry("800x500")
+
+
+    def buttonClick(self):
+        """ handle button click event and output text from entry area"""
+        self.submitButton.configure(bg = "green")
+
+
+if __name__ == "__main__":
+    guiFrame = GUI()
+    guiFrame.mainloop()
 #create window
-root = Tk()
+#root = Tk()
 
 #modify root window
-root.title("Exome Analyzer")
-root.geometry("800x500")
+#root.title("Exome Analyzer")
+#root.geometry("800x500")
 
-but = Button(root, text="Start Simulation", width=25)
-but.pack()
+#self.but = Button(root, text="Start Simulation", width=25, command=self.buttonClick)
+#self.but.configure(bg = "#009f9a")
+#self.but.pack()
 
-root.mainloop()
+#root.mainloop()
+
+
+
 
