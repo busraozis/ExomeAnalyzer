aea329ba98763b7d55ca62cd3aba858a01fa559c
bozis <busraozis1994@hotmail.com>
2017-04-01 15:58:44 +0300

    ilk pencere denemeleri

--- src/main.py
+++ src/main.py
@@ -18,12 +18,61 @@ class GUI(Tk):
 
     def buttonClick(self):
         """ handle button click event and output text from entry area"""
-        self.submitButton.configure(bg = "green")
+        self.toolScreen()
+
+    def toolScreen(self):
+        self.submitButton.destroy()
+        self.label1 = Label(self, text="1. FastQC", relief=RAISED, bg="#009f9a")
+        self.label1.place(x=150, y=150)
+        self.cbox1 = Checkbutton()
+        self.cbox1.place(x=220, y=150)
+        self.label2 = Label(self, text="2. BWA", relief=RAISED, bg="#009f9a")
+        self.label2.place(x=150, y=170)
+        self.cbox2 = Checkbutton(state=DISABLED)
+        self.cbox2.select()
+        self.cbox2.place(x=220, y=170)
+        self.label3 = Label(self, text="3. Picard", relief=RAISED, bg="#009f9a")
+        self.label3.place(x=150, y=190)
+        self.cbox3 = Checkbutton(state=DISABLED)
+        self.cbox3.select()
+        self.cbox3.place(x=220, y=190)
+        self.label4 = Label(self, text="4. Samtools", relief=RAISED, bg="#009f9a")
+        self.label4.place(x=150, y=210)
+        self.cbox4 = Checkbutton()
+        self.cbox4.place(x=220, y=210)
+        self.label5 = Label(self, text="4. GATK", relief=RAISED, bg="#009f9a")
+        self.label5.place(x=150, y=230)
+        self.cbox5 = Checkbutton()
+        self.cbox5.place(x=220, y=230)
+        self.label6 = Label(self, text="4. FreeBayes", relief=RAISED, bg="#009f9a")
+        self.label6.place(x=150, y=250)
+        self.cbox6 = Checkbutton()
+        self.cbox6.place(x=220, y=250)
+        self.label7 = Label(self, text="5. Annovar", relief=RAISED, bg="#009f9a")
+        self.label7.place(x=150, y=270)
+        self.cbox7 = Checkbutton(state=DISABLED)
+        self.cbox7.select()
+        self.cbox7.place(x=220, y=270)
+
+
+        self.button = Button(self, text="Add new tool", width=25, command=self.button2Click, bg="#009fff")
+        self.button.pack()
+
+        self.button2 = Button(self, text="Next -->", width=10, command=self.button2Click, bg="#009fff")
+        self.button2.pack(side="bottom")
+
+    def button2Click(self):
+        self.destroy()      ######################
+        Tk.__init__(self, master)
+        self.grid()
+
 
 
 if __name__ == "__main__":
     guiFrame = GUI()
     guiFrame.mainloop()
+
+
 #create window
 #root = Tk()
 
